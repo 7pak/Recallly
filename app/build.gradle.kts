bf8bae476ea95820d813ddffc26e97bca0a84cd2
abood <fatabdella7@gmail.com>
2026-03-22 05:48:54 +0400

    Bump versionCode to 3 for next Play Store upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
         applicationId = "com.at.recallly"
         minSdk = 28
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
